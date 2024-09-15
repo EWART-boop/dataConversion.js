3779f99a5db6c7d9651455255e72dc491574bf6d
EWART-boop <johnewartdelacruz@gmail.com>
2024-09-15 14:44:57 +0800

    dataConversion.js.

---
+++ app.js
@@ -0,0 +1,23 @@
+const price = '200';
+
+let number1 = parseInt(price);
+console.log(number1);
+
+let number2 = Number(price);
+console.log(number2);
+
+let number3 = +price;
+console.log(number3)
+
+let distance = '150';
+
+let distance1 =distance.toString();
+console.log(distance1);
+
+let distance2 = String(distance);
+console.log(distance2);
+
+let weight = '72.5';
+
+let decimalWeight = parseFloat(weight);
+console.log(decimalWeight);
